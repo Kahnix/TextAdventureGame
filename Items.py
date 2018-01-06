381558a2c1e67cc0f6c75b3fbcdba259371b5dc3
Brigdan <kacperdev@gmail.com>
2018-01-06 17:20:58 +0000

    Initial Commit, Test Stuff

---
+++ Items.py
@@ -0,0 +1,2 @@
+class Items:
+	def __init__(self):
---
+++ NonPlayable.py
@@ -0,0 +1,13 @@
+class NonPlayable:
+	def __init__(self, name, health, damage):
+		self.name = name
+		self.health = health
+		self.damage = damage
+
+	def __str__(self):
+		return \
+		"Name: {} \nHealth: {} \nDamage {}\n".format(self.name,self.health,self.damage)
+
+
+Miguel = NonPlayable("Miguel", 100, 10)
+Kacper = NonPlayable("Kacper", 100, 10)
