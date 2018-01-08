1a37f3d9ea69b2721d3d4f292021a81b1198ee5d
tiledman <migdfwoop@gmail.com>
2018-01-08 14:34:50 +0000

    Merged 'Heals' and 'Damages' class attributes into one

--- Items.py
+++ Items.py
@@ -1,15 +1,20 @@
 class Items:
-  def __init__(self, name, description, weight, heal, damage):
+  def __init__(self, name, description, weight, healthImpact):
     self.name = name
     self.description = description 
     self.weight = weight 
-    self.heal = heal
-    self.damage = damage
+    self.healthImpact = healthImpact
   
   def __str__(self):
-    return \
-    "Item name: " + self.name + "\n" \
-    + "Item description: " + self.description + "\n" \
-    + "Weight: " + str(self.weight) + "G\n" \
-    + "Heals: " + str(self.heal) + " HP\n" \
-    + "Damages: " + str(self.damage) + " HP"
+    if self.healthImpact >= 0:
+        return \
+        "Item name: " + self.name + "\n" \
+        + "Item description: " + self.description + "\n" \
+        + "Weight: " + str(self.weight) + "G\n" \
+        + "Heals: " + str(self.healthImpact) + " HP"
+    else:
+        return \
+        "Item name: " + self.name + "\n" \
+        + "Item description: " + self.description + "\n" \
+        + "Weight: " + str(self.weight) + "G\n" \
+        + "Damages: " + str(self.healthImpact) + " HP"
