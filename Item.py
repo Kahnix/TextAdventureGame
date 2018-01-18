20030941fccf796b26fa92f608db384e4a85790d
tiledman <migdfwoop@gmail.com>
2018-01-18 13:21:33 +0000

    Changed plural .py files to singular.

---
+++ Item.py
@@ -0,0 +1,28 @@
+class Item:
+  def __init__(self, name, description, weight, healthImpact, value):
+    self.name = name
+    self.description = description 
+    self.weight = weight 
+    self.healthImpact = healthImpact
+    self.value = value
+  
+  def __str__(self):
+    if self.healthImpact >= 0:
+        return \
+        "Name: {}\nDescription: {}\nCost: {}\nWeight: {}KG\nHeals: {}HP\n".format(self.name, self.description, self.weight, self.healthImpact)         
+    else:
+        return \
+        "Name: {}\nDescription: {}\nCost: {}\nWeight: {}KG\nDamages: {}HP\n".format(self.name, self.description, self.weight, self.healthImpact)
+
+class Inventory():
+    def __init__(self):
+        self.items={}
+
+    def __str__(self):
+        out = '\t'.join(['Name','Description of Item\t','KG','healthImpact','value'])
+        for item in self.items.values():
+            out += '\n' + '\t'.join([str(x) for x in [item.name, item.description, item.weight, item.healthImpact, item.value]])
+        return out
+ 
+    def addItem(self, item):
+        self.items[item.name] = item
