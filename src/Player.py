79bcbed4a71933105e153c6b0153ae667238582d
brigdan <kacperdev@gmail.com>
2018-02-21 10:30:34 +0000

    Refactored code for PEP8 and Python 3.6 format function

--- src/Player.py
+++ src/Player.py
@@ -1,47 +1,57 @@
 class Player:
-	def __init__(self,health,damage,hunger,thirst):
-		self.health = health
-		self.damage = damage
-		self.hunger = hunger
-		self.thirst = thirst
+    def __init__(self, health, damage, hunger, thirst):
+        self.health = health
+        self.damage = damage
+        self.hunger = hunger
+        self.thirst = thirst
 
-	def __str__(self):
-		return \
-		"Health: {}\nDamage: {}\nHunger: {}\nThirst: {}\n".format(self.health, self.damage, self.hunger, self.thirst)
+    def __str__(self):
+        return
+        f"Health: {self.health}\n \
+        Damage: {self.damage}\n \
+        Hunger: {self.hunger}\n \
+        Thirst: {self.thirst} \n"
 
-	def Eat(self, food):
-		if food.healthImpact > 0:
-			self.health += food.healthImpact
-			print("The {} healed you for {}HP. Your  HP is now {}").format(food.name, str(food.healthImpact), str(self.health))
-		else:
-			self.health += food.healthImpact
-			print("The {} was bad and damaged you for  {}HP. Your HP is now {}").format(food.name, str(food.healthImpact), str(self.health))
+    def Eat(self, food):
+        if food.healthImpact > 0:
+            self.health += food.healthImpact
+            print(f"The {food.name} healed you for {str(food.healthImpact)}HP.\
+            Your HP is now {str(self.health)}")
+        else:
+            self.health += food.healthImpact
+            print(f"The {food.name} was bad and damaged you for \
+            {str(food.healthImpact)}HP. Your HP is now {str(self.health)}")
 
-	def Attack(self,character):
-		self.healthafter=character.health - self.damage
+    def Attack(self, character):
+        self.healthafter = character.health - self.damage
 
-		if self.healthafter >0:
-			print("You attacked {}. He loses {}HP").format(character.name, str(self.damage))
-		else:
-			print("You've killed {}").format(character.name)
+        if self.healthafter > 0:
+            print(f"You attacked {characer.name}. \
+            He loses {str(self.damage)}HP")
+        else:
+            print(f"You've killed {characer.name}")
 
-	def Sleep(self):
-		if self.health <= 90:
-			self.health += 10
-			print("You rested and healed {}HP").format(10)
-		else:
-			print("You can't sleep now.")
+    def Sleep(self):
+        if self.health <= 90:
+            self.health += 10
+            print("You rested and healed {}HP").format(10)
+        else:
+            print("You can't sleep now.")
+
 
 class NonPlayer:
-	def __init__(self, name, health, damage):
-		self.name = name
-		self.health = health
-		self.damage = damage
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
 
-	def __str__(self):
-		return \
-		"Name: {} \nHealth: {} \nDamage {}\n".format(self.name,self.health,self.damage)
-	
-	def Attack(self, player):
-		player.health -= self.damage
-		print("{} attacked you for {}HP. You now have {}HP").format(self.name, str(self.damage), str(player.health))
+    def __str__(self):
+        return
+        f"Name: {self.name} \n \
+        Health: {self.health} \n \
+        Damage {self.damage}\n"
+
+    def Attack(self, player):
+        player.health -= self.damage
+        print(f"{self.name} attacked you for {str(self.damage)}HP.\
+        You now have {str(player.health}HP")
