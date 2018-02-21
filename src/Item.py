b8c38218b5f5908482b42bd6e5d305497910dfda
TiledMan <tiledman@github.com>
2018-02-21 15:51:05 +0000

    Fixed __str__ and changed __init__ parameter order

--- src/Item.py
+++ src/Item.py
@@ -1,24 +1,27 @@
 class Item:
-    def __init__(self, name, description, weight, healthImpact, value):
+    def __init__(self, name, description, value, weight, healthImpact):
         self.name = name
         self.description = description
+        self.value = value
         self.weight = weight
         self.healthImpact = healthImpact
-        self.value = value
 
     def __str__(self):
         if self.healthImpact >= 0:
-            print(f"Name: {self.name}\n\
-                    Description: {self.description}\n\
-                    Cost:\ {self.value}\n\
-                    Weight: {self.weight}KG\n\
-                    Heals: {self.healthImpact}HP\n")
+            return(f"\
+Name: {self.name}\n\
+Description: {self.description}\n\
+Cost: {self.value}\n\
+Weight: {self.weight}KG\n\
+Heals: {self.healthImpact}HP\n")
+
         else:
-            print(f"Name: {self.name}\n\
-                    Description: {self.description}\n\
-                    Cost: {self.value}\n\
-                    Weight: {self.weight}KG\n\
-                    Damages: {self.healthImpact}HP\n")
+            return(f"\
+Name: {self.name}\n\
+Description: {self.description}\n\
+Cost: {self.value}\n\
+Weight: {self.weight}KG\n\
+Damages: {self.healthImpact}HP\n")
 
 
 class Inventory():
@@ -28,6 +31,7 @@ class Inventory():
     def __str__(self):
         out = '\t'.join(['Name', 'Description of Item\t',
                          'KG', 'healthImpact', 'value'])
+
         for item in self.items.values():
             out += '\n' + '\t'.join([str(x) for x in [item.name,
                                                       item.description,
@@ -38,3 +42,7 @@ class Inventory():
 
     def addItem(self, item):
         self.items[item.name] = item
+
+
+thing = Item("BAD Food", "NOT Tasty", 15, 25, -20)
+print(thing)
