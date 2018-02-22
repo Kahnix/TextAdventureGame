96bc005fda6635e1756b1cfad6c7de5dad0e06bc
TiledMan <tiledman@github.com>
2018-02-22 11:08:34 +0000

    Completely overhauled Item class to be a parent class for others

--- src/Item.py
+++ src/Item.py
@@ -1,17 +1,52 @@
 class Item:
-    def __init__(self, name, description, weight, healthImpact):
+    def __init__(self, name, description, weight):
         self.name = name
-        self.description = description 
-        self.weight = weight 
-        self.healthImpact = healthImpact
-  
+        self.description = description
+        self.weight = weight
+
     def __str__(self):
-        if self.healthImpact >= 0:
-            return\
-            "Name: {}\nDescription: {}\nWeight: {}KG\n Heals: {}HP\n".format(self.name, self.description, self.weight, self.healthImpact)         
-        else:
-            return\
-            "Name: {}\nDescription: {}\nWeight: {}KG\nDamages: {}HP\n".format(self.name, self.description, self.weight, self.healthImpact)
+        return (f"\nName: {self.name}\nDescription: {self.description}\nWeight:{self.weight}")
 
     def __repr__(self):
         return str(self)
+
+class Consumable(Item):
+    def __init__(self, name, description, weight, healthImpact):
+        super().__init__(name, description, weight)
+        self.healthImpact = healthImpact
+
+    def __str__(self):
+        if self.healthImpact > 0:
+            return (f"{super(Consumable, self).__str__()}\nHeals: {self.healthImpact}")
+        else:
+            return (f"{super(Consumable, self).__str__()}\nDamages: {self.healthImpact}")
+
+    def consume(self, name, healthImpact):
+        return
+
+
+class Weapon(Item):
+    def __init__(self, name, description, weight, damage, damage_type):
+        super().__init__(name, description, weight)
+        self.damage = damage
+        self.damage_type = damage_type
+
+    def __str__(self):
+        return (f"{super(Weapon, self).__str__()}\nDamage: {self.damage}\nDamage Type: {self.damage_type}")
+
+
+def main():
+
+    class Sandwich(Consumable):
+        def __init__(self):
+            self.name = "Sandwich"
+            self.description = "A lovely BLT sandwich"
+            self.weight = 0.5
+            self.healthImpact = 10
+
+    sandwich1 = Sandwich()
+    print(sandwich1)
+
+
+if __name__ == '__main__':
+    main()
