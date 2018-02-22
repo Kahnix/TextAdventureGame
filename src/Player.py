3d8b71d86007452fe0312964ecb75ee1ef171971
brigdan <kacperdev@gmail.com>
2018-02-22 10:31:44 +0000

    Reworked inventory system to be exclusive to player.

--- src/Player.py
+++ src/Player.py
@@ -1,16 +1,21 @@
 class Player:
-    def __init__(self, health, damage, hunger, thirst):
+    def __init__(self, health, damage, hunger, thirst, inventory):
         self.health = health
         self.damage = damage
         self.hunger = hunger
         self.thirst = thirst
+        self.inventory = []
 
     def __str__(self):
         return
         f"Health: {self.health}\n \
         Damage: {self.damage}\n \
         Hunger: {self.hunger}\n \
-        Thirst: {self.thirst} \n"
+        Thirst: {self.thirst} \n \
+        Current Items: {self.inventory}"
+
+    def __repr__(self):
+        return str(self)
 
     def Eat(self, food):
         if food.healthImpact > 0:
@@ -31,10 +36,16 @@ class Player:
         else:
             print(f"You've killed {characer.name}")
 
+    def pickupItem(self, item):
+            self.inventory.append(item)
+
+    def checkInventory(self):
+            print(self.inventory)
+    
     def Sleep(self):
         if self.health <= 90:
             self.health += 10
-            print("You rested and healed {}HP").format(10)
+            print("You rested and healed 10HP")
         else:
             print("You can't sleep now.")
 
@@ -54,4 +65,4 @@ class NonPlayer:
     def Attack(self, player):
         player.health -= self.damage
         print(f"{self.name} attacked you for {str(self.damage)}HP.\
-        You now have {str(player.health}HP")
+        You now have {str(player.health)}HP")
